ebb0a078da02e2b7756eaa7d35bafc8a5dc7919b
Tiago dos Santos <tiago2904santos@gmail.com>
2026-05-10 16:39:39 -0300

    checkpoint antes de sanear pasta duplicada do projeto

--- oficios/apps.py
+++ oficios/apps.py
@@ -5,3 +5,7 @@ class OficiosConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "oficios"
     verbose_name = "Oficios"
+
+    def ready(self):
+        # Registra checagens de URLconf do wizard (oficios/checks.py).
+        from . import checks  # noqa: F401
